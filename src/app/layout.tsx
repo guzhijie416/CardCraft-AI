7525ccf6c6292761b00a96b20ef36553ccb0970b
Zhijie Gu <guzhijie@gmail.com>
2025-11-12 15:28:27 +0000

    Can font for Title like Create & Share.. chnaed to "Dreaming Outloud Pro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700&family=Pacifico&display=swap"
           rel="stylesheet"
         />
       </head>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        headline: ['Pacifico', 'cursive'],
         code: ['monospace'],
       },
       colors: {
